88103f78b3454a69ce5dc364dae7ea6ec1df0ed7
TheArlii <uzarli3989@gmail.com>
2025-06-29 17:34:39 +0500

    Create back.py

---
+++ keyboards/back.py
@@ -0,0 +1,9 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+# Umumiy orqaga tugmasi
+back_menu = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text="🔙 Orqaga")]
+    ],
+    resize_keyboard=True
+)
