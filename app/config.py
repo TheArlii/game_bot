4662aa6958bec25e2c88849081f269f8337f14ed
TheArlii <uzarli3989@gmail.com>
2025-06-29 18:59:46 +0500

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,5 +1,5 @@
+# config.py
 import os
-import json
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_IDS = json.loads(os.getenv("ADMIN_IDS", "[]"))
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+ADMIN_IDS = list(map(int, os.environ.get("ADMIN_IDS", "").split(",")))
