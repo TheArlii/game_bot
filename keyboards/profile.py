f9a7cc76dd5ede369b01655022609ca0e200eba0
TheArlii <uzarli3989@gmail.com>
2025-06-29 17:36:05 +0500

    Create profile.py

---
+++ keyboards/profile.py
@@ -0,0 +1,9 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+# Profil menyusi
+profile_menu = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text="🔙 Orqaga")]
+    ],
+    resize_keyboard=True
+)
