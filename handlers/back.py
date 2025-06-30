0961333a58b7f34fb6fd42334c617c59fc90877f
TheArlii <uzarli3989@gmail.com>
2025-06-30 09:31:57 +0500

    Update back.py

--- handlers/back.py
+++ handlers/back.py
@@ -8,16 +8,26 @@ router = Router()
 # 🔙 Orqaga tugmasi uchun handler
 @router.message(lambda m: m.text == "🔙 Orqaga")
 async def back_handler(message: Message, bot: Bot, state: FSMContext):
+    user_id = message.from_user.id
+
+    # 🔄 Kiruvchi va eski xabarlarni o‘chirish
     try:
-        await bot.delete_message(message.chat.id, message.message_id)
-    except: pass
+        await bot.delete_message(chat_id=message.chat.id, message_id=message.message_id)
+    except:
+        pass
 
     data = await state.get_data()
     msg_id = data.get("last_msg_id")
     if msg_id:
         try:
-            await bot.delete_message(message.chat.id, msg_id)
-        except: pass
+            await bot.delete_message(chat_id=message.chat.id, message_id=msg_id)
+        except:
+            pass
 
-    msg = await message.answer("🏠 Asosiy menyu:", reply_markup=main_menu)
+    # 🏠 Asosiy menyuni qayta yuborish
+    msg = await message.answer(
+        "🏠 Asosiy menyu:",
+        reply_markup=main_menu(user_id)
+    )
+
     await state.update_data(last_msg_id=msg.message_id)
